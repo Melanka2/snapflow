237a7f26d653c6c865d7cb317cec691f8d63f51a
nuwan <nuwandeshapriyaaa@gmail.com>
2024-04-09 16:53:12 +0530

    add hash pass and js

--- app/views/pages/signIn.php
+++ app/views/pages/signIn.php
@@ -6,10 +6,10 @@
 </head>
 <body>
     <h1 class="">Login</h1>
-    <form>
-        <input type="text" name="username" placeholder="Username">
+    <form method="post" action="<?php echo URLROOT; ?>/UserController/signIn">
+        <input type="text" name="email" placeholder="Email">
         <input type="password" name="password" placeholder="Password">
-        <button type="submit">Login</button>
+        <button type="submit" name="submitSignin">Login</button>
     </form>
 </body>
 </html>
