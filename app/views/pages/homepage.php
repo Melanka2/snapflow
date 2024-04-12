e75697a5b5616418a0c18b8f50156706589db11f
mayashi99 <mayashikodikara@gmail.com>
2024-04-13 03:49:54 +0530

    add search (homepage)

--- app/views/pages/homepage.php
+++ app/views/pages/homepage.php
@@ -6,32 +6,63 @@
 </head>
 <body>
 
-<img src="<?php echo URLROOT ?>/public/images/logo.png" alt="logo" class="logimg">
+
+<img src="<?php echo URLROOT ?>/public/images/logo new.png" alt="logo" class="logimg">
 
 <div class="topnav">
   <a class="active" href="#home">Home</a>
   <a href="#Blog">Blog</a>
   <a href="#About us">About Us</a>
   <a href="#Packages">Packages</a>
-</div>
-
-    
-    
     <div class="log">
     <a href="<?php echo URLROOT ?>/PageController/signup"><button class="Signup">Sign up</button></a>
     <a href="<?php echo URLROOT ?>/PageController/signIn"><button class="Login">Log in</button></a>
-</div><br>
+</div> 
+</div>
 
-<div class="image">
-  <img src="<?php echo URLROOT ?>/public/images/image 3.jpg" alt="girl">
-  <div class="content">
-    <h1>Welcome to SnapFlow</h1>
+<div class="center">
+  <img src="<?php echo URLROOT ?>/public/images/image 3.jpg" alt="girl" width="98%" height="700">
+  <div class="text-block_1">
+    <h1>Welcome to SnapFlow event photography</h1>
+</div>
+    <div class="text-block_2">
     <p><h3>We are a photography company, creating memorirs through lens...</h3></p>
   </div>
   <div class="centered">
-    <input type="text" placeholder="Search..">
+    
+    <label for="date">Select Date:</label>
+    <input type="date" id="date" name="date">
+
+    <label for="category">Select Category:</label>
+    <select id="category" name="category">
+      <option value="Wedding">Wedding</option>
+      <option value="Birthday">Birthday</option>
+      <option value="Get">Get</option>
+      <button type="submit">Search</button>
   </div>
 </div>
-<h3>Stories</h3>
+
+<h1>Stories</h1><br>
+
+<div class="row">
+  <div class="column">
+    <img src="<?php echo URLROOT ?>/public/images/image4.jpg" alt="wedding">
+    <img src="<?php echo URLROOT ?>/public/images/image_2.jpg" alt="rock">
+    <img src="<?php echo URLROOT ?>/public/images/image10.jpg" alt="uno">
+    
+  </div>
+  <div class="column">
+    <img src="<?php echo URLROOT ?>/public/images/image6.jpg" alt="uno">
+    <img src="<?php echo URLROOT ?>/public/images/image5.jpg" alt="ocun">
+  </div>
+
+    <div class="column">
+    <img src="<?php echo URLROOT ?>/public/images/image7.jpeg" alt="falls">
+    <img src="<?php echo URLROOT ?>/public/images/image8.jpg" alt="ocun" >
+  </div>
+    
+  </div>
+ 
+</div>
 </body>
 </html>
