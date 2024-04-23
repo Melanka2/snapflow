2db2aaa019c9054a62df743aa88b5b06d3950bca
mayashi99 <mayashikodikara@gmail.com>
2024-04-23 14:07:50 +0530

    add signup

--- app/views/pages/signup.php
+++ app/views/pages/signup.php
@@ -5,15 +5,13 @@
       <link rel="stylesheet" href="<?php echo URLROOT ?>/public/css/style_signup.css">
 </head>
 <body>
-    <center>
     <h1 >Signup</h1>
     <form method="post" action="<?php echo URLROOT; ?>/UserController/signUp">
-
-    <img class="img-user" src="<?php echo URLROOT ?>/public/img/userlogo.png" alt="user">
-    <input type="text" id="Name" name="Name" placeholder='user name'>
-
+        <input type="text" name="email" placeholder="email">
+        <input type="password" name="password" placeholder="Password">
+        <button name="submitSignup" value="submitSignup">Signup</button>
     </form>
-</center>
+
 <script src="<?php echo URLROOT ?>/public/js/form.js"></script>
 </body>
 </html>
