1ff046b30add8e4a60fbc48405deb8d7fce8de89
mayashi99 <mayashikodikara@gmail.com>
2024-04-24 21:35:19 +0530

    add photo

--- app/views/pages/feedback.php
+++ app/views/pages/feedback.php
@@ -27,7 +27,7 @@
         <div>
             <a href="<?php echo URLROOT ?>/PageController/homepage">Home</a>
             <a href="<?php echo URLROOT ?>/PageController/blog">Blog</a>
-            <a href="<?php echo URLROOT ?>/PageController/about us">About Us</a>
+            <a href="<?php echo URLROOT ?>/PageController/about">About Us</a>
             <a href="<?php echo URLROOT ?>/PageController/packages">Packages</a>
         </div>
         <div class="log">
