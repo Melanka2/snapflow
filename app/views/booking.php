33991e91a8a1b5d50e4e81180c75e198354cb803
Nimna-Silva <nimnasilva23@gmail.com>
2024-04-26 04:08:14 +0530

    add booking page

---
+++ app/views/booking.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<head>
+	<title>Payment Successful</title>
+	
+	<link rel="stylesheet" href="<?php echo URLROOT ?>/public/css/style_booking.css">
+</head>
+<body>
+	
+	<form style="background-color:#E6E6FA">
+	<fieldset>
+	<div class="padding">
+	<table table-width="100%">
+		<tr>
+			<td>
+				<lable>First Name</lable>
+				<input type="text" value="(Amila)">
+				<br><br>
+	
+				<lable>Last Name</lable>
+				<input type="text" value="(Fernando)">
+				<br><br>
+	
+				<lable>Email</lable>
+				<input type="text" value="Email Address">
+				<br><br>
+	
+				<lable>Phone Number</lable>
+				<input type="text" value="+94">
+				<br><br>
+				
+				<lable>Package Type</lable>
+				<select>
+					<option value="pack1">Package 1</option>
+					<option value="pack2">Package 2</option>
+					<option value="pack3">Package 3</option>
+				</select>
+				<br><br>
+				
+				<lable>Photographer Name</lable>
+				<select>
+					<option value="p1">Lakshan</option>
+					<option value="p2">Kasun</option>
+					<option value="p3">Malith</option>
+				</select>
+				<br><br>
+				
+				<lable>Date</lable>
+				<input type="date">
+				<br><br>
+				
+				<lable>Venue</lable>
+				<input type="text" value="Location">
+				<br><br>
+				
+				<input type="submit" value="Proceed">
+				<br><br>
+			</td>
+		</tr>
+	</table>
+	</div>
+	</fieldset>
+	</form>	
+</body>
+</html>
