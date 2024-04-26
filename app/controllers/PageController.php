2bc1ab8ed75a336f7a54edc3f7cfcd1636de211a
Nimna-Silva <nimnasilva23@gmail.com>
2024-04-27 04:54:46 +0530

    add booking 1 page

--- app/controllers/PageController.php
+++ app/controllers/PageController.php
@@ -87,7 +87,12 @@ class PageController extends Controller
         $this->view('pages/booking');
     }
 
-
+    public function booking1()
+    {
+        $this->view('pages/booking1');
+    }
 
    
+
+    
 }
