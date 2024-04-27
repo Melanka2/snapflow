5e67d63d3fee5cb9821a30b1c12eeed8ae260ea0
mayashi99 <mayashikodikara@gmail.com>
2024-04-27 09:15:56 +0530

    add signup

--- app/views/pages/signup.php
+++ app/views/pages/signup.php
@@ -1,31 +1,17 @@
 <!DOCTYPE html>
 <html>
-
 <head>
     <title>Signup</title>
-    <link rel="stylesheet" href="<?php echo URLROOT ?>/public/css/style_signup.css">
+      <link rel="stylesheet" href="<?php echo URLROOT ?>/public/css/style_signup.css">
 </head>
-
 <body>
+    <h1 >Signup</h1>
+    <form method="post" action="<?php echo URLROOT; ?>/UserController/signUp">
+        <input type="text" name="email" placeholder="email">
+        <input type="password" name="password" placeholder="Password">
+        <button name="submitSignup" value="submitSignup">Signup</button>
+    </form>
 
-    <div class="signup-container">
-        <form id="signup-form" method="post" action="<?php echo URLROOT; ?>/UserController/signUp">
-            <h2>Sign Up</h2>
-            <div class="input-group">
-                <label for="username">Username:</label>
-                <input type="text" name="email" placeholder="email">
-            </div>
-
-            <div class="input-group">
-                <label for="password">Password:</label>
-                <input type="password" name="password" placeholder="Password">
-            </div>
-            <button name="submitSignup" value="submitSignup">Signup</button>
-        </form>
-        </form>
-    </div>
-
-    <script src="<?php echo URLROOT ?>/public/js/form.js"></script>
+<script src="<?php echo URLROOT ?>/public/js/form.js"></script>
 </body>
-
 </html>
